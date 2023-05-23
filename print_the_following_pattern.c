1d23c1bc70dc085bfc146dca9c037617e762502b
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-05-23 07:30:01 +0530

    #include<stdio.h> int main() {     int i, j,n;     scanf("%d", &n);
        for(i=1; i<=n; i++)
        {
            for(j=1; j<=i; j++)
            {
                printf("%d", j);
            }
    
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-  int r,o,i1,i2;
-  int n=1;
-  scanf("%d",&r);
-  for(o=0;o<r;o++)
-       {
-       for(i1=r-1;i1>o;i1--)
-       {
-            printf(" ");
-       }
-       for(i2=0;i2<n;i2++)
-       {
-           printf("%d",n-o);
-       }
-       n+=2;
-       printf("
+    int i, j,n;
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("
 ");
-       }
+    }
 }
