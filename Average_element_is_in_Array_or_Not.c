1c66d5931a6ab110a2e2f775e93e51a3e3d23da4
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-09-23 15:03:52 +0530

    #include<stdio.h> int main() {     int a[100],i,n,c=0,avg,f=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=c+a[i];     }     avg=c/n;     for(i=0;i<n;i++)     {         if(avg==a[i])         {             f++;         }     }     if(f==0)     {         printf("False");     }     else     {         printf("True");     } }

---
+++ Average_element_is_in_Array_or_Not.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,n,c=0,avg,f=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=c+a[i];
+    }
+    avg=c/n;
+    for(i=0;i<n;i++)
+    {
+        if(avg==a[i])
+        {
+            f++;
+        }
+    }
+    if(f==0)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
